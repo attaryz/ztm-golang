95d91b3f70dc5b9ef2c58098f92e1b4ec4bb09dd
attaryz <markosberg123@gmail.com>
2022-08-20 21:54:26 +0300

    pointers

--- src/lectures/demo/pointers/pointers.go
+++ src/lectures/demo/pointers/pointers.go
@@ -2,6 +2,38 @@ package main
 
 import "fmt"
 
+type Counter struct {
+	hits int
+}
+
+func increment(counter *Counter) {
+	counter.hits += 1
+
+	fmt.Println("Counter", counter)
+}
+
+func replace(old *string, new string, counter *Counter) {
+	*old = new
+	increment(counter)
+}
+
 func main() {
 
+	counter := Counter{}
+
+	hello := "hello"
+	world := "world"
+
+	fmt.Println("hello", hello)
+	fmt.Println("world", world)
+
+	replace(&hello, "hi", &counter)
+	fmt.Println(hello, world)
+
+	phrase := []string{hello, world}
+
+	fmt.Println(phrase)
+
+	replace(&phrase[1], "yes", &counter)
+	fmt.Println(phrase)
 }
--- src/lectures/exercise/pointers/pointers.go
+++ src/lectures/exercise/pointers/pointers.go
@@ -18,6 +18,49 @@ package main
 
 import "fmt"
 
+const (
+	Active   = true
+	Inactive = false
+)
+
+type SecurityTag bool
+
+type Item struct {
+	name string
+	tag  SecurityTag
+}
+
+func activate(tag *SecurityTag) {
+	*tag = Active
+}
+func deactivate(tag *SecurityTag) {
+	*tag = Inactive
+}
+
+func checkout(items []Item) {
+	fmt.Println("Checking out...")
+	for i := 0; i < len(items); i++ {
+		deactivate(&items[i].tag)
+	}
+}
+
 func main() {
+
+	shirt := Item{"shirt", Active}
+	pants := Item{"pants", Active}
+	shoes := Item{"shoes", Active}
+	socks := Item{"socks", Active}
+	watch := Item{"watch", Active}
+
+	items := []Item{shirt, pants, shoes, socks, watch}
+
+	fmt.Println(items)
+
+	deactivate(&items[0].tag)
+
+	fmt.Println("deactivated items: ", items)
+
+	checkout(items)
+	fmt.Println("checked out items: ", items)
 
 }
