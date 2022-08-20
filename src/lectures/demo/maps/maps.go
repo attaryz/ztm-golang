48264afa0c2af577826b8fb7a7c5cb1041d08c52
attaryz <markosberg123@gmail.com>
2022-08-20 21:26:50 +0300

    maps

--- src/lectures/demo/maps/maps.go
+++ src/lectures/demo/maps/maps.go
@@ -3,4 +3,37 @@ package main
 import "fmt"
 
 func main() {
+
+	shoppingList := make(map[string]int)
+	shoppingList["apples"] = 5
+	shoppingList["oranges"] = 10
+	shoppingList["bananas"] = 15
+	shoppingList["bread"] += 3
+	shoppingList["bananas"] += 1
+
+	fmt.Println(shoppingList)
+
+	delete(shoppingList, "apples")
+	fmt.Println("apples got deleted", shoppingList)
+	fmt.Println("need", shoppingList["oranges"], "oranges")
+
+	cereal, found := shoppingList["cereal"]
+	if found {
+		fmt.Println("found", cereal)
+	} else {
+		fmt.Println("cereal not found")
+	}
+
+	milk, found := shoppingList["milk"]
+	if !found {
+		fmt.Println("milk not found shopping list has", shoppingList)
+	} else {
+		fmt.Println("milk found", milk)
+	}
+
+	totalItems := 0
+	for _, amount := range shoppingList {
+		totalItems += amount
+	}
+	fmt.Println("total items", totalItems)
 }
