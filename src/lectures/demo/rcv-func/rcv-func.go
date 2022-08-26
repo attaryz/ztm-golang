94e1afd23631ed1b7fed6e579ce8639d359dc040
attaryz <markosberg123@gmail.com>
2022-08-26 23:32:23 +0300

    receiver functions

--- src/lectures/demo/rcv-func/rcv-func.go
+++ src/lectures/demo/rcv-func/rcv-func.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+type Space struct {
+	occupied bool
+}
+
+type ParkingLot struct {
+	spaces []Space
+}
+
+func occupySpace(lot *ParkingLot, spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingLot) occupySpaceReceiver(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+func (lot *ParkingLot) vacateSpaceReceiver(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
+}
+
 func main() {
+
+	lot := ParkingLot{spaces: make([]Space, 5)}
+
+	fmt.Println("Initial Space: ", lot)
+
+	lot.occupySpaceReceiver(1)
+	lot.occupySpaceReceiver(5)
+	occupySpace(&lot, 2)
+	fmt.Println("After Occupy: ", lot)
+	lot.vacateSpaceReceiver(1)
+	fmt.Println("After vacate: ", lot)
 
 }
--- src/lectures/exercise/rcv-func/rcv-func.go
+++ src/lectures/exercise/rcv-func/rcv-func.go
@@ -16,6 +16,59 @@ package main
 
 import "fmt"
 
+type Player struct {
+	name              string
+	health, maxHealth uint
+	energy, maxEnergy uint
+}
+
+func (player *Player) addHealth(amount uint) {
+	player.health += amount
+	if player.health > player.maxHealth {
+		player.health = player.maxHealth
+	}
+	fmt.Println(player.name, "Add", amount, "health=>", player.health)
+}
+
+func (player *Player) applyDamage(amount uint) {
+	if player.health-amount > player.health {
+		player.health = 0
+	} else {
+		player.health -= amount
+	}
+	fmt.Println(player.name, "Apply", amount, "=>", player.health)
+}
+func (player *Player) addEnergy(amount uint) {
+	player.energy += amount
+	if player.energy > player.maxEnergy {
+		player.energy = player.maxEnergy
+	}
+	fmt.Println(player.name, "Add", amount, "energy=>", player.energy)
+}
+
+func (player *Player) consumeEnergy(amount uint) {
+	if player.energy-amount > player.energy {
+		player.energy = 0
+	} else {
+		player.energy -= amount
+	}
+	fmt.Println(player.name, "Apply", amount, "=>", player.energy)
+}
+
 func main() {
 
+	player := Player{
+		name:      "knight",
+		health:    100,
+		maxHealth: 100,
+		energy:    100,
+		maxEnergy: 1000,
+	}
+
+	player.applyDamage(50)
+	player.addHealth(30)
+	player.addEnergy(500)
+	player.consumeEnergy(50)
+
+	fmt.Println(player)
 }
